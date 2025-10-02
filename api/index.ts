2c93f0f462c13a78fc40407a900b66152a1da3f8
Friday Godwin <feelsmez@gmail.com>
2025-10-02 12:22:33 +0100

    Add Vercel runtime config to api function
    - Export config with nodejs20.x runtime
    - Include Prisma client files in the config
    - Remove functions config from vercel.json

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,11 @@ import { installGlobals } from "@remix-run/node";
 
 installGlobals();
 
+export const config = {
+  runtime: 'nodejs20.x',
+  includeFiles: ['node_modules/.prisma/client/**']
+};
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Dynamically import the server build
   const build = await import("../build/server/index.js");
